1acb57491e5f7ae76734172681f6670c3bc9ccb4
unknown <arumansalik45@gmail.com>
2026-01-31 18:57:26 +0530

    Subject Grid Added

---
+++ components/dashboard/SubjectGrid.tsx
@@ -0,0 +1,26 @@
+const subjects = [
+    {
+        code: "CS8491",
+        name: "Computer Architecture",
+    },
+    {
+        code: "CS8451",
+        name: "Design and Analysis of Algorithms",
+    },
+    {
+        code: "CS8492",
+        name: "Database Management Systems",
+    },
+];
+
+export default function SubjectGrid() {
+    return (
+        <div>
+            <h3>
+                Your Subjects
+            </h3>
+
+
+        </div>
+    );
+}
